4d600a5acc69d0b543ba17c192eba32718d4a4fd
Daniel Baird <danielbaird@Daniels-MacBook-Pro.local>
2026-06-17 20:17:42 -0700

    Other

--- skull/agents/dan.py
+++ skull/agents/dan.py
@@ -2,7 +2,7 @@ import random
 
 from skull.agents.base import SkullAgent
 from skull.state import ObservableState
-from skull.actions import Action
+from skull.actions import Action, ActionType
 
 
 class DanBot(SkullAgent):
@@ -21,11 +21,15 @@ class DanBot(SkullAgent):
         self.last_action_say_say = True
         return Action.say("I'm the joker...")
 
+    def return_non_say_action(self, action):
+        self.last_action_say_say = False
+        if action.action_type() == ActionType.SAY:
+            raise Exception("Attempting to say a non-say action" + action)
+        return action
+
     def choose_action(self, state: ObservableState, valid_actions: list[Action]) -> Action:
         if not self.last_action_say_say:
             return self.return_say_action()
-
-        state
 
         self.last_action_say_say = False
         return self.rng.choice(valid_actions)
