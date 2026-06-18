c20d864641f85df06a528608e959c5b43d5a675b
Daniel Baird <danielbaird@Daniels-MacBook-Pro.local>
2026-06-17 20:43:55 -0700

    Other

--- skull/agents/dan.py
+++ skull/agents/dan.py
@@ -22,7 +22,7 @@ class DanBot(SkullAgent):
 
     def return_normal_action(self, action, valid_actions):
         self.last_action_say_say = False
-        if action.action_type() == ActionType.SAY:
+        if action.action_type == ActionType.SAY:
             raise Exception("Attempting to say a non-say action" + action)
         if action is None:
             return self.rng.choice(valid_actions)
